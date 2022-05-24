32582f6c33581c39826d897e300875fe9b36e491
Jakub <jakub.garczarek@gmail.com>
2022-05-24 20:59:05 +0200

    bez niepotrzebnego argumentu

--- pdf_scraper.py
+++ pdf_scraper.py
@@ -3,7 +3,7 @@ import tabula as tb
 file = 'http://geoportal.pgi.gov.pl/css/surowce/images/2020/bilans_2020.pdf'
 # file = 'bilans_2020.pdf' #do testów lokalnych
 
-df = tb.read_pdf(file, pages = '170-175',columns=[3])
+df = tb.read_pdf(file, pages = '170-175')
 print(df[0])
 # tylko konkretne kolumny:
 # print(df[0][df.columns.get_loc('Nazwa złoża')]) 
