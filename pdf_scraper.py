11f020ed665f0eec7e0892caaeb85035857fc9ea
Jakub <jakub.garczarek@gmail.com>
2022-05-25 19:50:35 +0200

    piaski i żwiry do słownika i pliku -kilka stron na próbę

--- pdf_scraper.py
+++ pdf_scraper.py
@@ -3,9 +3,23 @@ import tabula as tb
 file = 'http://geoportal.pgi.gov.pl/css/surowce/images/2020/bilans_2020.pdf'
 # file = 'bilans_2020.pdf' #do testów lokalnych
 
-df = tb.read_pdf(file, pages = '170-175')
-print(df[0])
-# tylko konkretne kolumny:
-# print(df[0][df.columns.get_loc('Nazwa złoża')]) 
-# print(df[0]['Wydo-\rbycie'])
+df = tb.read_pdf(file, pages = '162-406')#zakres stron w bilansie dla  piasków i żwirów
+
+piaski_zwiry = {}
+page = 0
+while (page <= 2):#liczba stron z podanego zakresu do skrapowania
+    for index, row in df[page].iterrows():
+        piaski_zwiry[row[2]]=[row[3],row[4], row[5], row[6], row[7]]
+    page += 1
+        
+print(piaski_zwiry)
+
+f = open('piaski_zwiry.txt','w')
+f.write(str(piaski_zwiry))
+f.close()
+
+
+
+
+
 
